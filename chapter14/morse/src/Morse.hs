4687b6fbc041e6203184b4b909d249b401cb548c
Mukesh Soni <msoni@planview.com>
2016-12-19 14:49:34 +0530

    chapter 14 - morse code partial

--- chapter14/morse/src/Morse.hs
+++ chapter14/morse/src/Morse.hs
@@ -11,5 +11,57 @@ import qualified Data.Map as M
 
 type Morse = String
 
+letterToMorse = M.fromList [
+      ('a', ".-")
+    , ('b', "-...")
+    , ('c', "-.-.")
+    , ('d', "-..")
+    , ('e', ".")
+    , ('f', "..-.")
+    , ('g', "--.")
+    , ('h', "....")
+    , ('i', "..")
+    , ('j', ".---")
+    , ('k', "-.-")
+    , ('l', ".-..")
+    , ('m', "--")
+    , ('n', "-.")
+    , ('o', "---")
+    , ('p', ".--.")
+    , ('q', "--.-")
+    , ('r', ".-.")
+    , ('s', "...")
+    , ('t', "-")
+    , ('u', "..-")
+    , ('v', "...-")
+    , ('w', ".--")
+    , ('x', "-..-")
+    , ('y', "-.--")
+    , ('z', "--..")
+    , ('1', ".----")
+    , ('2', "..---")
+    , ('3', "...--")
+    , ('4', "....-")
+    , ('5', ".....")
+    , ('6', "-....")
+    , ('7', "--...")
+    , ('8', "---..")
+    , ('9', "----.")
+    , ('0', "-----")
+    ]
 
+moreseToLetter :: M.Map Morse Char
+morseToLetter =
+    M.foldWithKey (flip M.insert) M.empty letterToMorse
+
+charToMorse :: Char -> Maybe Morse
+charToMorse c = M.lookup c letterToMorse
+
+stringToMorse :: String -> Maybe [Morse]
+stringToMorse s = 
+    sequence $ fmap charToMorse s
+
+morseToChar :: Morse -> Maybe Char
+morseToChar m =
+    M.lookup m morseToLetter
 
