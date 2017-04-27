385f220b506651a1531d48a49e787303c24c2396
Mukesh Soni <mukeshsoni@gmail.com>
2017-04-27 22:55:26 +0530

    added few more quickcheck tests

--- chapter14/arithprops/test/Spec.hs
+++ chapter14/arithprops/test/Spec.hs
@@ -8,5 +8,26 @@ prop_half :: (Eq a, Fractional a) => a -> Bool
 prop_half n = 
     n == halfIdentity n
 
+prop_plusAssociative x y z =
+    x + (y + z) == (x + y) + z
+
+prop_plusCommutative x y =
+    x + y == y + x
+
+prop_mulAssociative x y z = 
+    x * (y * z) == (x * y) * z
+
+prop_mulCommutative x y =
+    x * y == y * x
+
+prop_powerAssociative x y z =
+    x ^ (y ^ z) == (x ^ y) ^ z
+
 main :: IO ()
-main = quickCheck (prop_half :: Float -> Bool)
+main = do
+    quickCheck (prop_half :: Float -> Bool)
+    quickCheck (prop_plusAssociative :: Int -> Int -> Int -> Bool)
+    quickCheck (prop_plusCommutative :: Int -> Int -> Bool)
+    quickCheck (prop_mulAssociative :: Int -> Int -> Int -> Bool)
+    quickCheck (prop_mulCommutative :: Int -> Int -> Bool)
+    quickCheck (prop_powerAssociative :: Int -> Int -> Int -> Bool)
