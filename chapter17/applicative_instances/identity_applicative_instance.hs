77dfae0212b1c83021618002041bc6b67db36779
Mukesh Soni <mukeshsoni@gmail.com>
2018-01-10 07:30:29 +0530

    chapter 17 - added applicative instance for Identity

---
+++ chapter17/applicative_instances/identity_applicative_instance.hs
@@ -0,0 +1,9 @@
+newtype Identity a = Identity a
+    deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+    fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+    pure = Identity
+    (<*>) (Identity f) = fmap f 
