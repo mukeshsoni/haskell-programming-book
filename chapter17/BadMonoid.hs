1a606a58bb626bd70495f8e29d115c145754ba7c
Mukesh Soni <mukeshsoni@gmail.com>
2018-01-12 22:06:35 +0530

    chapter 17 - trying out checkers library for quickcheck for easy testing of laws

---
+++ chapter17/BadMonoid.hs
@@ -0,0 +1,23 @@
+module BadMonoid where
+
+import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+data Bull =
+    Fools
+  | Twoo
+  deriving (Eq, Show)
+
+instance Arbitrary Bull where
+    arbitrary =
+        frequency [(1, return Fools), (1, return Twoo)]
+
+instance Monoid Bull where
+    mempty = Fools
+    mappend _ _ = Fools
+
+instance EqProp Bull where (=-=) = Eq
+
+main = quickBatch (monoid Twoo)
