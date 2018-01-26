c32911cfa0208a7e5b3e96271b4b67540c625ed0
Mukesh Soni <mukeshsoni@gmail.com>
2018-01-26 10:19:42 +0530

    chapter 19 - successfully copied the url shortener code from the book :D

--- chapter 19/shawty/src/Main.hs
+++ chapter 19/shawty/src/Main.hs
@@ -24,6 +24,68 @@ randomElement s = do
 shortyGen :: IO [Char]
 shortyGen = replicateM 7 $ randomElement alphaNum
 
+saveURI :: R.Connection
+        -> BC.ByteString
+        -> BC.ByteString
+        -> IO (Either R.Reply R.Status)
+saveURI conn shortURI uri =
+  R.runRedis conn $ R.set shortURI uri
+
+getURI :: R.Connection
+       -> BC.ByteString
+       -> IO (Either R.Reply (Maybe BC.ByteString))
+getURI conn shortURI = R.runRedis conn $ R.get shortURI
+
+linkShorty :: String -> String
+linkShorty shorty =
+  concat [ "<a href=\""
+         , shorty
+         , "\">Copy and paste your short URL</a>"
+         ]
+  
+shortyCreated :: Show a => a -> String -> TL.Text
+shortyCreated resp shawty =
+  TL.concat [ TL.pack (show resp)
+            , " shorty is: ", TL.pack (linkShorty shawty)
+            ]
+
+shortyAintUri :: TL.Text -> TL.Text
+shortyAintUri uri =
+  TL.concat [ uri
+            , " wasn't a url. did you forget http://"
+            ]
+
+shortyFound :: TL.Text -> TL.Text
+shortyFound tbs =
+  TL.concat ["<a href=\"", tbs, "\">", tbs, "</a>"]
+
+app :: R.Connection -> ScottyM ()
+app rConn = do
+  get "/" $ do
+    uri <- param "uri"
+    let parsedUri :: Maybe URI
+        parsedUri = parseURI (TL.unpack uri)
+    case parsedUri of
+      Just _ -> do
+        shawty <- liftIO shortyGen
+        let shorty = BC.pack shawty
+            uri' = encodeUtf8 (TL.toStrict uri)
+        resp <- liftIO (saveURI rConn shorty uri')
+        html (shortyCreated resp shawty)
+      Nothing -> text (shortyAintUri uri)
+  get "/:short" $ do
+    short <- param "short"
+    uri <- liftIO (getURI rConn short)
+    case uri of
+      Left reply -> text (TL.pack (show reply))
+      Right mbBS -> case mbBS of
+        Nothing -> text "uri not found"
+        Just bs -> html (shortyFound tbs)
+          where tbs :: TL.Text
+                tbs = TL.fromStrict (decodeUtf8 bs)
+
 main :: IO ()
+
 main = do
-  putStrLn "hi sir"
+  rConn <- R.connect R.defaultConnectInfo
+  scotty 3000 $ app rConn
