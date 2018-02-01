a12e9de7a8aa97d2e488c58e0796dccdf2200bcb
Mukesh Soni <mukeshsoni@gmail.com>
2018-02-01 10:10:18 +0530

    chapter 22 - warm up exercise

---
+++ chapter22/warming_up.hs
@@ -0,0 +1,13 @@
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap = fmap toUpper
+
+rev :: [Char] -> [Char]
+rev = reverse
+
+composed :: [Char] -> [Char]
+composed = rev . cap
+
+fmapped :: [Char] -> [Char]
+fmapped = fmap rev cap
