1c659a091b239ae005d5d3305b1f497dbbdbd628
Mukesh Soni <mukeshsoni@gmail.com>
2018-02-06 15:53:59 +0530

    chapter 24 - learning parsers

---
+++ chapter24/learning-parsers/src/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import Text.Trifecta
+
+stop :: Parser a
+stop = unexpected "stop"
+
+one :: Parser Char
+one = char '1'
+
+one' = one >> stop
+
+oneTwo :: Parser Char
+oneTwo = char '1' >> char '2'
+
+oneTwo' = oneTwo >> stop
+
+testParse :: Parser Char -> IO ()
+testParse p =
+  print $ parseString p mempty "123"
+
+pNL s =
+  putStrLn ('\n' : s)
+
+main :: IO ()
+main = do
+  pNL "stop:"
+  testParse stop
+  pNL "one:"
+  testParse one
+  pNL "one':"
+  testParse one'
+  pNL "oneTwo:"
+  testParse oneTwo
+  pNL "oneTwo':"
+  testParse oneTwo'
+
