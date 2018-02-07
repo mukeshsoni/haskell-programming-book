8a7d384cecc65fef1e703396683021f426034ea1
Mukesh Soni <mukeshsoni@gmail.com>
2018-02-07 10:23:26 +0530

    chatper 24 - completed the learning parsers exercises

--- chapter24/learning-parsers/src/Main.hs
+++ chapter24/learning-parsers/src/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
 import Text.Trifecta
+import Text.Parser.Combinators
 
 stop :: Parser a
 stop = unexpected "stop"
 
 one :: Parser Char
 one = char '1'
+two = char '2'
+three = char '3'
 
 one' = one >> stop
 
@@ -18,6 +21,26 @@ oneTwo' = oneTwo >> stop
 testParse :: Parser Char -> IO ()
 testParse p =
   print $ parseString p mempty "123"
+
+testEOF :: Parser () -> IO ()
+testEOF p =
+  print $ parseString p mempty "123"
+
+testString' :: Parser String -> IO ()
+testString' p =
+  print $ parseString p mempty "123"
+
+oneString :: Parser String
+oneString = string "1"
+
+oneTwoString :: Parser String
+oneTwoString = string "12"
+
+oneTwoThreeString :: Parser String
+oneTwoThreeString = string "123"
+
+-- oneTwoThreeString' :: Parser String
+-- oneTwoThreeString' = string "123" <|> string "12" <|> string "1"
 
 pNL s =
   putStrLn ('\n' : s)
@@ -34,4 +57,15 @@ main = do
   testParse oneTwo
   pNL "oneTwo':"
   testParse oneTwo'
-
+  pNL "oneTwoStop:"
+  testEOF (oneTwo >> eof)
+  pNL "oneString"
+  testString' oneString
+  pNL "oneTwoString"
+  testString' oneTwoString
+  pNL "oneTwoThreeString"
+  testString' oneTwoThreeString
+  pNL "oneTwoThreeString'"
+  testString' $ choice [oneTwoThreeString, oneTwoString, oneString]
+  pNL "oneTwoThreeChar'"
+  testString' $ choice [one >> two >> three, one >> two, one]
